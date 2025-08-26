3270880c64d11b0ff8a0238e86ea600ac1cdf3c9
stevan <stevan.lezaja@gmail.com>
2025-08-26 14:29:43 +0200

    getters for m and km

--- custom_types/units.py
+++ custom_types/units.py
@@ -43,6 +43,15 @@ class Length(Unit):
     def __init__(self, value: float, unit: str):
         super().__init__(value=value, unit=unit)
 
+    @property
+    def m(self):
+        return self._value
+
+    @property
+    def km(self):
+        return self._value/1e-3
+
+
 if __name__ == "__main__":
     distance = Length(value=10, unit='km')
     print(distance)
