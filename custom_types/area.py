e938c42f19b694e9d4582dc22c4a1b7e2b22f0c0
stevan <stevan.lezaja@gmail.com>
2025-08-26 15:13:48 +0200

    initial area implementation

---
+++ custom_types/area.py
@@ -0,0 +1,36 @@
+from .units import Unit
+
+
+class Area(Unit):
+    default_unit = 'm^2'
+    degree = 2
+
+    def __init__(self, value: float, unit: str):
+        super().__init__(value=value, unit=unit)
+
+    @property
+    def m(self):
+        return self._value
+    
+    @m.setter
+    def m(self, new):
+        self.value = (new, 'm^2')
+
+    @property
+    def um(self):
+        return self._value / 1e-12
+
+    @um.setter
+    def um(self, new):
+        self.value = (new, 'um^2')
+
+
+if __name__ == '__main__':
+    a = Area(1, 'mm^2')
+    print(a)
+
+    a.m = 10
+    print(a)
+
+    a.um = 15
+    print(a)
