be5b2642689075f1c0aa959b42bc2a8df7d0a228
stevan <stevan.lezaja@gmail.com>
2025-08-26 21:44:58 +0200

    added Power to custom types

--- custom_types/__init__.py
+++ custom_types/__init__.py
@@ -1,3 +1,4 @@
 from .length import Length
 from .area import Area
 from .frequency import Frequency
+from .power import Power
---
+++ custom_types/power.py
@@ -0,0 +1,17 @@
+from .units import Unit
+
+
+class Power(Unit):
+    default_unit = 'W'
+
+    def __init__(self, value: float, unit: str):
+        super().__init__(value=value, unit=unit)
+
+    @property
+    def W(self):
+        return self._value
+
+    @W.setter
+    def W(self, new):
+        self.value = (new, 'W')
+
