363c4996e980afe1c4501b73d91fe37f70c04360
stevan <stevan.lezaja@gmail.com>
2025-09-07 12:23:55 +0200

    mW setter and getter of power

--- custom_types/power.py
+++ custom_types/power.py
@@ -15,3 +15,11 @@ class Power(Unit):
     def W(self, new):
         self.value = (new, 'W')
 
+    @property
+    def mW(self):
+        return self._value
+
+    @mW.setter
+    def mW(self, new):
+        self.value = (new, 'mW')
+
