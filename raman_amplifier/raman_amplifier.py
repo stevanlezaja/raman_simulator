3b7adecfd0384c451b822fe43f124a84858c9199
stevan <stevan.lezaja@gmail.com>
2025-09-07 12:25:35 +0200

    raman amplifier pumping ratio validation

--- raman_amplifier/raman_amplifier.py
+++ raman_amplifier/raman_amplifier.py
@@ -7,11 +7,15 @@ class Pump:
         self.power = power
 
 
+def _validate_ratio(pumping_ratio: float):
+    if pumping_ratio < 0 or pumping_ratio > 1.0:
+        raise ValueError("Pumping ratio can only be in range [0.0, 1.0]")
+
+
 class RamanAmplifier:
     def __init__(self, pumping_ratio: float=1.0):
-        if pumping_ratio < 0 or pumping_ratio > 1.0:
-            raise ValueError("Pumping ratio can only be in range [0.0, 1.0]")
-        self.pumping_ratio = pumping_ratio
+        _validate_ratio(pumping_ratio)
+        self._pumping_ratio = pumping_ratio
         self._pump_power = Power(0.5, 'W')
         self.pump_wavelength = Length(1455, 'nm')
         self.forward_pump = Pump(Power(self._pump_power.W * self.pumping_ratio, 'W'), self.pump_wavelength)
