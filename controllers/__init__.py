2307439e812e4085ddfc95d10bf470dcae81f6f9
stevan <stevan.lezaja@gmail.com>
2025-09-21 12:54:43 +0200

    controller module imports controller protocol

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -3,6 +3,7 @@
 # from .policy_gradient_controler import PolicyGradientControler
 # from .inverse_controller import InverseController
 from .pid_controller import PidController
+from .controller_base import Controller
 # from .controller import Controller, ControllerType, controller_step
 
-__all__ = ["PidController"]
+__all__ = ["PidController", "Controller"]
