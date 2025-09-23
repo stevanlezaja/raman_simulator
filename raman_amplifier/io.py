8d7e8439c5f88f952781fa9a2351914e30b18c87
stevan <stevan.lezaja@gmail.com>
2025-09-23 21:12:06 +0200

    formatting and testing io.py

--- raman_amplifier/io.py
+++ raman_amplifier/io.py
@@ -1,12 +1,28 @@
+"""
+This module contains Input and Output types for a Raman Amplifier
+"""
+
 from typing import TypeVar, Generic, Type, Iterator, Optional
 
 import custom_types as ct
 
 
 class RamanInputs:
-    def __init__(self, powers: Optional[list[ct.Power]] = None, wavelengths: Optional[list[ct.Length]] = None, n_pumps: Optional[int] = None):
+    """
+    RamanInputs is a class used to represent inputs to a Raman Amplifier
+        It contains the Wavelength - Power pairs representing the Pump state
+    """
+    def __init__(
+            self,
+            powers: Optional[list[ct.Power]] = None,
+            wavelengths: Optional[list[ct.Length]] = None,
+            n_pumps: Optional[int] = None
+        ):
         if powers is not None or wavelengths is not None:
-            assert powers is not None and wavelengths is not None, "Both powers and wavelengths need to be provided"
+            assert (
+                powers is not None and
+                wavelengths is not None
+            ), "Both powers and wavelengths need to be provided"
 
         if powers is None:
             assert n_pumps is not None, "n_pumps cannot be None if powers are None"
@@ -18,12 +34,15 @@ class RamanInputs:
 
         self.wavelengths: list[ct.Length] = wavelengths
         self.powers: list[ct.Power] = powers
-
+        self.value_dict: dict[ct.Length, ct.Power] = dict(zip(wavelengths, powers))
 
 T = TypeVar("T", bound = ct.UnitProtocol)
 
 class Spectrum(Generic[T]):
-
+    """
+    Spectrum is a generic class that represents values over Frequencies
+        Id consists of a spectrum dict that stores the pairs of Frequency and value
+    """
     operations = ['+', '-']
 
     def __init__(self, value_cls: Type[T]):
@@ -31,9 +50,23 @@ class Spectrum(Generic[T]):
         self.spectrum: dict[ct.Frequency, T] = {}
 
     def _linear_op(self, other: "Spectrum[T]", operation: str) -> "Spectrum[T]":
+        """
+        Method that implements linear operations on Spectrum objects
+            Operation is passed as a string and matched to a class variable list
+            containing the supported operations.
+            When implementing support for a new operation, add its symbol to the list.
+        """
+
         assert operation in Spectrum.operations, f"Operation {operation} is not supported"
-        assert isinstance(other, Spectrum), (f"Both operands need to be type {self.__class__.__name__}")
-        assert self.spectrum.keys() == other.spectrum.keys(), ("Both spectra need to have the same frequencies")
+
+        assert (
+            isinstance(other, Spectrum)
+        ), (f"Both operands need to be type {self.__class__.__name__}")
+
+        assert (
+            self.spectrum.keys() == other.spectrum.keys()
+        ), ("Both spectra need to have the same frequencies")
+
         result: Spectrum[T] = Spectrum(self.value_cls)
         for f, g in self.spectrum.items():
             if operation == '+':
@@ -54,12 +87,18 @@ class Spectrum(Generic[T]):
 
     @property
     def mean(self) -> float:
+        """
+        Mean property retuns the mean value over the spectrum
+        """
         mean: float = 0.0
         for v in self.spectrum.values():
             mean += v.value
         return mean/len(self.spectrum.values())
 
     def add_val(self, frequency: ct.Frequency, value: T) -> None:
+        """
+        Adds a Frequency - value pair to the Spectrum dict
+        """
         self.spectrum[frequency] = value
 
     def __iter__(self) -> Iterator[tuple[ct.Frequency, T]]:
@@ -67,30 +106,14 @@ class Spectrum(Generic[T]):
 
     @property
     def frequencies(self):
+        """"
+        Property that returns the Frequencies in a spectrum as a list
+        """
         return list(self.spectrum.keys())
 
     @property
     def values(self):
+        """
+        Property that retuns the spectrum values as a list
+        """
         return list(self.spectrum.values())
-
-
-if __name__ == "__main__":
-    spec1 = Spectrum(ct.PowerGain)
-    spec1.spectrum = {
-        ct.Frequency(0, 'Hz'): ct.PowerGain(10, ''),
-        ct.Frequency(1, 'Hz'): ct.PowerGain(15, ''),
-        ct.Frequency(2, 'Hz'): ct.PowerGain(20, ''),
-        ct.Frequency(3, 'Hz'): ct.PowerGain(25, ''),
-        ct.Frequency(4, 'Hz'): ct.PowerGain(7, ''),
-    }
-
-    spec2 = Spectrum(ct.PowerGain)
-    spec2.spectrum = {
-        ct.Frequency(0, 'Hz'): ct.PowerGain(1, ''),
-        ct.Frequency(1, 'Hz'): ct.PowerGain(2, ''),
-        ct.Frequency(2, 'Hz'): ct.PowerGain(3, ''),
-        ct.Frequency(3, 'Hz'): ct.PowerGain(4, ''),
-        ct.Frequency(4, 'Hz'): ct.PowerGain(1, ''),
-    }
-
-    print(spec1 + spec2)
