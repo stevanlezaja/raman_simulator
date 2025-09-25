eebc2faa9213f0c056d6088680f925f765f1568f
stevan <stevan.lezaja@gmail.com>
2025-09-25 10:31:03 +0200

    signal property setters and getters

--- signals/signals.py
+++ signals/signals.py
@@ -1,7 +1,29 @@
-from custom_types import Length, Power
+import custom_types as ct
 
 
 class Signal:
     def __init__(self):
-        self.power = Power(0.1, 'mW')
-        self.wavelength = Length(1470, 'nm')
+        self._power = ct.Power(0.1, 'mW')
+        self._wavelength = ct.Length(1470, 'nm')
+
+    @property
+    def power(self):
+        return self._power
+
+    @power.setter
+    def power(self, new: ct.Power | tuple[float, str]):
+        if isinstance(new, ct.Power):
+            self._power = new
+            return
+        self._power = new
+
+    @property
+    def wavelength(self):
+        return self._wavelength
+
+    @wavelength.setter
+    def wavelength(self, new: ct.Length | tuple[float, str]):
+        if isinstance(new, ct.Length):
+            self._wavelength = new
+            return
+        self._wavelength = new
