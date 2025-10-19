8a77e72106310eaf3933d73f79e3d7068a22cf4a
stevan <stevan.lezaja@gmail.com>
2025-10-19 12:15:05 +0200

    bernoulli controller linting

--- controllers/bernoulli_controler/__init__.py
+++ controllers/bernoulli_controler/__init__.py
@@ -1,1 +1,24 @@
+"""
+controllers.bernoulli_controller package
+========================================
+
+This package provides the BernoulliController for Raman Amplifier control.
+
+The `BernoulliController` implements a stochastic, Bernoulli-based
+reinforcement learning policy to adjust pump powers and wavelengths
+based on observed gain spectra.
+
+Modules
+-------
+bernoulli_controller
+    Contains the BernoulliController class and its implementation.
+
+Exports
+-------
+BernoulliController
+    The main controller class available for external use.
+"""
+
 from .bernoulli_controler import BernoulliController
+
+__all__ = ['BernoulliController']
