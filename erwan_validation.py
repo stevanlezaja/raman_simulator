646ab708fadb27b868b4758a08cbdda026a79607
stevan <stevan.lezaja@gmail.com>
2025-11-22 16:33:52 +0100

    parameters

--- erwan_validation.py
+++ erwan_validation.py
@@ -14,7 +14,7 @@ def main():
     input_spectrum = ra.Spectrum(ct.Power)
     for num in list(np.linspace(1530, 1560, 40)):
         freq = conv.wavelenth_to_frequency(ct.Length(num, 'nm'))
-        input_spectrum.add_val(freq, ct.Power(25, 'uW'))
+        input_spectrum.add_val(freq, ct.Power(250, 'uW'))
 
 
     num_pumps = 3
