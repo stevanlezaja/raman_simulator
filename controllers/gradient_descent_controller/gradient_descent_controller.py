c950c2fff844b4896ec0951b1b598c677bc196f4
stevan <stevan.lezaja@gmail.com>
2025-11-23 09:30:24 +0100

    gd controller init

--- controllers/gradient_descent_controller/gradient_descent_controller.py
+++ controllers/gradient_descent_controller/gradient_descent_controller.py
@@ -7,9 +7,13 @@ from utils.loading_data_from_file import load_raman_dataset
 from ..controller_base import Controller
 from .forward_nn import ForwardNN
 
-class GradientDescentController(Controller, torch.nn.Module):
-    def __init__(self):
-        pass
+
+class GradientDescentController(Controller):
+    def __init__(self, lr_model: float = 1e-3, lr_control: float = 5e-2):
+        super().__init__()
+        self.model = ForwardNN()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr_model)
+        self.control_lr = lr_control
 
     def train_controller(self, file_path: str):
         pass
