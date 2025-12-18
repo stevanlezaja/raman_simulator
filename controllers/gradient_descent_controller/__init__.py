b2e82c8adc72c207761ca90716cc63a023db7d76
stevan <stevan.lezaja@gmail.com>
2025-12-18 21:58:38 +0100

    added bw model

--- controllers/gradient_descent_controller/__init__.py
+++ controllers/gradient_descent_controller/__init__.py
@@ -1,3 +1,7 @@
 from .gradient_descent_controller import GradientDescentController
+from .forward_nn import ForwardNN
+from .backward_nn import BackwardNN
+from .train_models import train_backward_model, train_forward_model
 
-__all__ = ['GradientDescentController']
+__all__ = ['GradientDescentController', 'ForwardNN', 'BackwardNN',
+           'train_backward_model', 'train_forward_model']
