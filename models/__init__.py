36709752a371698690a5aa2a827068b816dce006
stevan <stevan.lezaja@gmail.com>
2025-12-23 10:26:16 +0100

    blank slate for rpm

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,5 @@
 from .backward_nn import BackwardNN
 from .backward_ensemble_nn import BackwardEnsemble
 from .forward_nn import ForwardNN
-from .backward_rpm import BackwardRPM
 
-__all__ = ['BackwardNN', 'ForwardNN', 'BackwardEnsemble', 'BackwardRPM']
+__all__ = ['BackwardNN', 'ForwardNN', 'BackwardEnsemble']
